d84dc2044282dff7fb1bb34daa8017dfa41469fc
nmsohn <odna425@gmail.com>
2025-04-18 17:09:13 +1200

    feat(client): Add calendar

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,15 @@
 import { Grid2 } from '@mui/material'
+import ActivityFilters from './ActivityFilters'
 import ActivityList from './ActivityList'
 
 export default function ActivityDashboard() {
     return (
         <Grid2 container spacing={3}>
-            <Grid2 size={7}>
+            <Grid2 size={8}>
                 <ActivityList />
             </Grid2>
-            <Grid2 size={5}>
-                Activity Filters
+            <Grid2 size={4}>
+                <ActivityFilters />
             </Grid2>
         </Grid2>
     )
