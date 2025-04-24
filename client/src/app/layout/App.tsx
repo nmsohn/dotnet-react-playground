5e72c5487605d6b081e031d6064f1a048b2e5ab8
nmsohn <odna425@gmail.com>
2025-04-24 20:58:44 +1200

    feat(client): Add scroll restoration

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import { Box, Container, CssBaseline } from '@mui/material'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router'
+import { Outlet, ScrollRestoration } from 'react-router'
 import { useLocation } from 'react-router'
 import HomePage from '../../features/home/HomePage';
 function App() {
@@ -11,6 +11,7 @@ function App() {
       bgcolor: "#eeeeee",
       minHeight: "100vh"
     }}>
+      <ScrollRestoration />
       <CssBaseline />
       {location.pathname === "/" ? <HomePage /> : (<>
         <NavBar />
