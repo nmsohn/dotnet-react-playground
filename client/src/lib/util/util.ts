43a096ef1aefc8dbf6cb73dc6a668e888c36f4e9
nmsohn <odna425@gmail.com>
2025-04-28 23:18:34 +1200

    feat(client, comment): Add comment component

--- client/src/lib/util/util.ts
+++ client/src/lib/util/util.ts
@@ -1,4 +1,4 @@
-import { DateArg, format } from "date-fns";
+import { DateArg, format, formatDistanceToNow } from "date-fns";
 import { z } from "zod";
 
 export function formatDate(date: DateArg<Date>) {
@@ -8,3 +8,7 @@ export function formatDate(date: DateArg<Date>) {
 export const requiredString = (fieldName: string) => z
     .string({ required_error: `${fieldName} is required` })
     .min(1, { message: `${fieldName} is required` });
+
+export function timeAgo(date: DateArg<Date>) {
+  return formatDistanceToNow(date) + " ago"
+}
